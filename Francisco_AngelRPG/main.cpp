c6acd8a913c0bda1fc59ffdb17a560a67a0c714b
Angel Francisco <angelfrancisco5252@gmail.com>
2025-03-30 22:22:49 -0700

    Relocated the .txt files so that they would be successfully loaded without having to input the entire relative path.

--- Francisco_AngelRPG/main.cpp
+++ Francisco_AngelRPG/main.cpp
@@ -5,13 +5,7 @@ using namespace std;
 int main() {
     GameDecisionTree<Story> myGame;
 
-    //StoryFile.txt relative path
-    ///Users/angelfrancisco/CLionProjects/programming-assignment-2-angelfranciscoo/Francisco_AngelRPG/StoryFile.txt
-
-    //story.txt relative path
-    ///Users/angelfrancisco/CLionProjects/programming-assignment-2-angelfranciscoo/Francisco_AngelRPG/story.txt
-
-    string filename = "/Users/angelfrancisco/CLionProjects/programming-assignment-2-angelfranciscoo/Francisco_AngelRPG/StoryFile.txt";     // Specify the input file
+    string filename = "StoryFile.txt";     // Specify the input file
     char delimiter = '|';     // '|' is the delimiter, change if needed
 
     // Load the story data from the file
